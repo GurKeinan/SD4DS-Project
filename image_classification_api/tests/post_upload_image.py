74fa9ede5de90874d5c10c9c68d11411d775c6cf
IdanPipano <102733803+IdanPipano@users.noreply.github.com>
2024-09-10 00:24:06 +0300

    write how i ran on machine + add 1 test

--- image_classification_api/tests/post_upload_image.py
+++ image_classification_api/tests/post_upload_image.py
@@ -6,8 +6,11 @@ class TestUploadImage(BaseAPITest):
     def setUp(self):
         super().setUp()
         self.endpoint = self.BASE_URL + '/upload_image'
+        self.test_image_png = 'tests/assets/britney.png'
+        self.test_image_jpeg = 'tests/assets/test_img.jpeg'
+
     def test_it_returns_200_when_image_is_uploaded(self):
-        test_files = [('tests/assets/test_img.jpeg', 'image/jpeg'), ('tests/assets/britney.png', 'image/png')]
+        test_files = [(self.test_image_jpeg, 'image/jpeg'), (self.test_image_png, 'image/png')]
         for file, MIME_type in test_files:
             with self.subTest(file=file):
                 with open(file, 'rb') as f:
@@ -16,28 +19,28 @@ class TestUploadImage(BaseAPITest):
                     print(response.json())
                 self.assertEqual(response.status_code, 200)
 
-    # def test_successful_upload_png(self):
-    #     filenames = ["test_image.png", "somepic.png", "image with spaces.png", "image_with_ünîçødé.png"]
-    #
-    #     for filename in filenames:
-    #         with self.subTest(filename=filename):
-    #             with open(self.test_image_png, 'rb') as image_file:
-    #                 files = {'image': (filename, image_file, 'image/png')}
-    #                 response = requests.post(self.upload_url, files=files)
-    #
-    #             self.assertEqual(response.status_code, 200)
-    #             self.assertEqual(response.headers['Content-Type'], 'application/json')
-    #
-    #             data = response.json()
-    #             self.assertIn('matches', data)
-    #             self.assertIsInstance(data['matches'], list)
-    #             for match in data['matches']:
-    #                 self.assertIn('name', match)
-    #                 self.assertIn('score', match)
-    #                 self.assertIsInstance(match['name'], str)
-    #                 self.assertIsInstance(match['score'], float)
-    #                 self.assertGreater(match['score'], 0.0)
-    #                 self.assertLessEqual(match['score'], 1.0)
+    def test_successful_upload_png(self):
+        filenames = ["test_image.png", "somepic.png", "image with spaces.png", "image_with_ünîçødé.png"]
+
+        for filename in filenames:
+            with self.subTest(filename=filename):
+                with open(self.test_image_png, 'rb') as image_file:
+                    files = {'image': (filename, image_file, 'image/png')}
+                    response = requests.post(self.endpoint, files=files)
+
+                self.assertEqual(response.status_code, 200)
+                self.assertEqual(response.headers['Content-Type'], 'application/json')
+
+                data = response.json()
+                self.assertIn('matches', data)
+                self.assertIsInstance(data['matches'], list)
+                for match in data['matches']:
+                    self.assertIn('name', match)
+                    self.assertIn('score', match)
+                    self.assertIsInstance(match['name'], str)
+                    self.assertIsInstance(match['score'], float)
+                    self.assertGreater(match['score'], 0.0)
+                    self.assertLessEqual(match['score'], 1.0)
 
 
 if __name__ == '__main__':
