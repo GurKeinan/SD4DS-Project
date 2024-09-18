3041a2e486b4ac903410ddb386cadc618857dc5a
IdanPipano <102733803+IdanPipano@users.noreply.github.com>
2024-09-18 16:37:56 +0300

    make another docker-compose for test

--- face_merge_quiz/app/__init__.py
+++ face_merge_quiz/app/__init__.py
@@ -5,12 +5,17 @@ from flask_login import LoginManager, current_user
 from gradio_client import Client
 import httpx  # Import httpx
 import os
+import logging
+logging.basicConfig(level=logging.INFO)
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'  # TODO change this
-app.config['MONGO_URI'] = 'mongodb://face-merge-mongodb:27017/face_merge_db'
+app.config['MONGO_URI'] = os.environ.get('MONGO_URI')  # MongoDB URI
+# app.config['FLASK_ENV'] = os.environ.get('FLASK_ENV')  # TODO: we need this?
 app.config['UPLOAD_FOLDER'] = 'app/static/uploads/'
 app.config['OUTPUT_FOLDER'] = 'app/static/outputs/'
+
+logging.info(f"MongoDB URI: {app.config['MONGO_URI']}")
 
 os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
 os.makedirs(app.config['OUTPUT_FOLDER'], exist_ok=True)
--- face_merge_quiz/run_tests.sh
+++ face_merge_quiz/run_tests.sh
@@ -1,10 +1,28 @@
-# To run the tests outside the container
-python -m unittest discover tests
+#FLASK_ENV=testing \
+#MONGO_URI=mongodb://face-merge-mongodb:27017/testdb \
+#docker-compose \
+#-f docker-compose.override.yml \
+#up
 
-# in terminal run:
-# chmod +x run_tests.sh  # to make the script executable
-# ./run_tests.sh
+docker-compose -f docker-compose.test.yml up
 
 
-# "python -m unittest discover <dir>" runs all tests in <dir>> (including subdirectories),
-# but recognizes only files whose names start with "test".
+
+
+
+
+
+
+
+
+
+## To run the tests outside the container
+#python -m unittest discover tests
+#
+## in terminal run:
+## chmod +x run_tests.sh  # to make the script executable
+## ./run_tests.sh
+#
+#
+## "python -m unittest discover <dir>" runs all tests in <dir>> (including subdirectories),
+## but recognizes only files whose names start with "test".
