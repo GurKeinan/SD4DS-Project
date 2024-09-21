4b206f25da8529ee998559154f56afeb7ddb1703
IdanPipano <102733803+IdanPipano@users.noreply.github.com>
2024-09-21 15:14:38 +0300

    add @login_required to /api/status

--- face_merge_quiz/app/api_routes.py
+++ face_merge_quiz/app/api_routes.py
@@ -1,9 +1,19 @@
 import requests
 from . import app, mongo, bcrypt, login_manager
+from flask_login import login_required
+from .models import User
+from bson.objectid import ObjectId
 
 
-
+@login_manager.user_loader
+def load_user(user_id):
+    user_data = mongo.db.users.find_one({"_id": ObjectId(user_id)})
+    if user_data:
+        return User(user_data['username'], user_data['password'], user_data['wins'], user_data['losses'],
+                    str(user_data['_id']))
+    return None
 @app.route('/api/status')
+@login_required
 def api_status():
     response = requests.get('http://face-merge-image-classification-api:6000/status')
     return "The response from our API server is:" + response.text, response.status_code, response.headers.items()
