a022b1fa826a1bca50016a3a852257e3be97611a
IdanPipano <102733803+IdanPipano@users.noreply.github.com>
2024-09-28 12:45:44 +0300

    Replace all "docker-compose" command with "docker compose"

--- face_merge_quiz/run_tests.sh
+++ face_merge_quiz/run_tests.sh
@@ -1,18 +1,4 @@
-#FLASK_ENV=testing \
-#MONGO_URI=mongodb://face-merge-mongodb:27017/testdb \
-#docker-compose \
-#-f docker-compose.override.yml \
-#up
+docker compose -f docker-compose.test.yml up --build --abort-on-container-exit
 
-docker-compose -f docker-compose.test.yml up --build --abort-on-container-exit
-
-## To run the tests outside the container
-#python -m unittest discover tests
-#
-## in terminal run:
-## chmod +x run_tests.sh  # to make the script executable
-## ./run_tests.sh
-#
-#
-## "python -m unittest discover <dir>" runs all tests in <dir>> (including subdirectories),
-## but recognizes only files whose names start with "test".
+# Note: if needed, run the following command to make this script executable
+# chmod +x run_tests.sh
--- image_classification_api/run_tests.sh
+++ image_classification_api/run_tests.sh
@@ -1,7 +1,7 @@
 # Run tests that check the image classification API
 
 # Start containers in detached mode
-docker-compose up -d
+docker compose up -d
 
 # Wait for 3 seconds to give the containers time to start
 sleep 3
@@ -10,7 +10,7 @@ sleep 3
 python -m unittest discover
 
 # Stop and remove the containers
-docker-compose down
+docker compose down
 
 
 # Note: if needed, run the following command to make this script executable
